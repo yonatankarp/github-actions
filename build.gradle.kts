7b7b5bc15413b4f02e049621b76e4d2a2f93154f
Yonatan Karp-Rudin <yonvata@gmail.com>
2023-12-13 17:28:02 +0100

    Add jar release pipeline (#11)
    Add release jar workflow
    
    This commit adds the required setup to release an artifact from the project into GitHub packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,33 @@
 plugins {
+    java
+    `maven-publish`
     id("com.diffplug.spotless") version "6.23.3"
 }
 
 repositories {
     mavenCentral()
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.yonatankarp"
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/yonatankarp/github-actions")
+                credentials {
+                    username = project.findProperty("gpr.user")?.toString() ?: System.getenv("GITHUB_ACTOR")
+                    password = project.findProperty("gpr.key")?.toString() ?: System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
 }
